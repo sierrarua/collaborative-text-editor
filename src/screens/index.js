8fc2fcb8eb213811bf766256838fec59642cfeb8
Viola Lee <31656705+violakyoungalee@users.noreply.github.com>
2018-07-18 21:26:19 -0400

    Delete index.js

--- src/screens/index.js
+++
@@ -1,4 +0,0 @@
-export {default as DocumentList} from './DocumentList'
-export {default as Document} from './Document'
-export {default as Connecting} from './Connecting'
-export {default as Login} from './Login'
