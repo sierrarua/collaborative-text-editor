6ffda4aafaf7862ce9ea9f4353b77c9e86693357
violakyoungalee <kyounga.lee@yale.edu>
2018-07-19 10:06:25 -0400

    some more

--- src/screens/Document.jsx
+++ src/screens/Document.jsx
@@ -155,10 +155,10 @@ export default class Document extends Component {
 
       <button onClick={this.onSave}>save</button>
       <button onClick={this.onExit}>exit</button>
-      <button onClick={this.onRevisionHistory}view revision history</button>
+      <button>view history</button>
       <div>
         <Search htmlFor="Find in Document"></Search>
-        <button onClick={this.onRevisionHistory}Search</button>
+        <button>Search</button>
       </div>
       <div className="draft-editor-container">
         <Editor
