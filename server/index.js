cc5da5d12daf608db3008b89ef2d88ea4ba74205
Sierra <jiang26s@mtholyoke.edu>
2018-07-19 12:05:52 -0700

    remove old routes

--- server/index.js
+++ server/index.js
@@ -14,60 +14,6 @@ io.on('connection', function (socket) {
 })
 
 app.use(routes)
-app.post('/register', (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    password: req.body.password,
-
-  });
-  newUser.save({}, (error, results) => {
-    if (error) {
-      console.log('error', error);
-      res.json({ error });
-    } else {
-      res.send(results);
-    }
-  });
-});
-
-app.post('/login', passport.authenticate('local'), (req, res) => {
-  console.log('LOGIN: ', req.user.username);
-  res.json({ success: true });
-});
-
-app.post('/newDoc/:userid', (req, res) => {
-  const newDoc = new Document({
-    owner: req.params.userid,
-    title: req.body.title,
-    password: req.body.password,
-    collaboratorList: [req.params.userid],
-    createdTime: Date.now(),
-  });
-  newDoc.save({}, (error) => {
-    if (error) {
-      console.log('error', error);
-    } else {
-      User.findById(req.params.userid)
-      .then((user) => {
-        user.docLists.push(newDoc);
-        user.save();
-        console.log(user);
-        res.json({ success: true });
-      })
-      .catch((err) => {
-        console.log('ERROR in loading a doc: ', err)
-        res.json({ success: false })
-      })
-    }
-  });
-});
-
-
-
-app.get('/doc', (req, res) => {
-  Document.findById(req.body.id)
-})
-;
 
 server.listen(1337, '127.0.0.1');
 
